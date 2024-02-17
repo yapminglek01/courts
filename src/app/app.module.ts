61914fe78b4026089f7499f7d74f5ab9ab6004a3
yapminglek <52651378+yapminglek@users.noreply.github.com>
2024-02-17 11:07:25 +0800

    h_page
    home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +15,20 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ProductTilesComponent } from './component/product-tiles/product-tiles.component';
 import { ProductComponent } from './product/product.component';
+import { MainComponent } from './main/main.component';
+import { CarouselComponent } from './component/carousel/carousel.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { ProductCategoryComponent } from './component/product-category/product-category.component';
+import { MatCardModule } from '@angular/material/card';
+import { AboutComponent } from './about/about.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +36,28 @@ import { ProductComponent } from './product/product.component';
     HeaderComponent,
     FooterComponent,
     ProductTilesComponent,
-    ProductComponent
+    ProductComponent,
+    MainComponent,
+    CarouselComponent,
+    ProductCategoryComponent,
+    AboutComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatGridListModule,
+    MatCardModule,
+    MatExpansionModule
+
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
