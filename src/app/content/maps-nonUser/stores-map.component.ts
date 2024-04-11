919631108405c9488daa87222fad5adf2c49bfd9
kevintan01 <115685030+kevintan01@users.noreply.github.com>
2024-04-11 23:15:06 +0800

    google map can show with the marker

--- src/app/content/maps-nonUser/stores-map.component.ts
+++ src/app/content/maps-nonUser/stores-map.component.ts
@@ -11,36 +11,29 @@ import { GoogleMapsModule, MapInfoWindow, MapMarker } from '@angular/google-maps
   styleUrls: ['./stores-map.component.css']
 })
 export class storesMapNonUser implements OnInit {
-    
-  constructor() {}
-      
-  ngOnInit(): void {}
-
-  display: any;
-  center: google.maps.LatLngLiteral = {
-      lat: 3.1319,
-      lng: 101.6841
-  };
-  zoom = 11;
-
-  moveMap(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.center = (event.latLng.toJSON());
+    center: google.maps.LatLngLiteral = { lat: 3.1319, lng: 101.6841 };
+    zoom = 12;
+    markerOptions: google.maps.MarkerOptions = { draggable: false };
+    markers: google.maps.LatLngLiteral[] = [
+      { lat: 3.235420, lng: 101.644630 }, //Selayang
+      { lat: 3.193840, lng: 101.605540 }, //Sri Damansara
+      { lat: 3.037820, lng: 101.550660 }, //Shah Alam
+      { lat: 3.200540, lng: 101.718090 }, //Setapak
+      { lat: 3.052034, lng: 101.781237 }, //Cheras
+      { lat: 3.009600, lng: 101.599340 }, //Puchong
+    ];
+  
+    constructor() {}
+  
+    ngOnInit(): void {}
+  
+    moveMap(event: google.maps.MapMouseEvent) {
+      if (event.latLng) {
+        this.center = event.latLng.toJSON();
+      }
+    }
+  
+    onMapReady(event: google.maps.Map) {
+      console.log('Adding markers...');
+    }
   }
-
-  move(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.display = event.latLng.toJSON();
-  }
-
-  addMarker(map: google.maps.Map) {
-      const marker = new google.maps.Marker({
-          position: { lat: 3.1481, lng: 101.6164 }, // Set the desired latitude and longitude
-          map: map,
-          title: 'My Marker'
-      });
-  }
-
-  onMapReady(event: any) {
-      const map = event.target as google.maps.Map;
-      this.addMarker(map);
-  }
-}
