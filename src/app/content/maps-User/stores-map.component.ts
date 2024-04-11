9d0bf1d3e0cbabf631e670d0d99173b3fcab338a
kevintan01 <115685030+kevintan01@users.noreply.github.com>
2024-04-11 23:16:06 +0800

    update with login user

--- src/app/content/maps-User/stores-map.component.ts
+++ src/app/content/maps-User/stores-map.component.ts
@@ -12,33 +12,29 @@ import { GoogleMapsModule } from '@angular/google-maps'
   styleUrls: ['./stores-map.component.css']
 })
 export class storesMapUser implements OnInit {
-    
+  center: google.maps.LatLngLiteral = { lat: 3.1319, lng: 101.6841 };
+  zoom = 12;
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  markers: google.maps.LatLngLiteral[] = [
+    { lat: 3.235420, lng: 101.644630 }, //Selayang
+    { lat: 3.193840, lng: 101.605540 }, //Sri Damansara
+    { lat: 3.037820, lng: 101.550660 }, //Shah Alam
+    { lat: 3.200540, lng: 101.718090 }, //Setapak
+    { lat: 3.052034, lng: 101.781237 }, //Cheras
+    { lat: 3.009600, lng: 101.599340 }, //Puchong
+  ];
+
   constructor() {}
-      
+
   ngOnInit(): void {}
 
-  display: any;
-  center: google.maps.LatLngLiteral = {
-      lat: 3.1319,
-      lng: 101.6841
-  };
-  zoom = 12;
-
-  /*------------------------------------------
-  --------------------------------------------
-  moveMap()
-  --------------------------------------------
-  --------------------------------------------*/
   moveMap(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.center = (event.latLng.toJSON());
+    if (event.latLng) {
+      this.center = event.latLng.toJSON();
+    }
   }
 
-  /*------------------------------------------
-  --------------------------------------------
-  move()
-  --------------------------------------------
-  --------------------------------------------*/
-  move(event: google.maps.MapMouseEvent) {
-      if (event.latLng != null) this.display = event.latLng.toJSON();
+  onMapReady(event: google.maps.Map) {
+    console.log('Adding markers...');
   }
 }
